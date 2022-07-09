f54282dc63f1af6189f3523f32a9519b5ecb42c1
Marcus Ong <58963565+marcusbory@users.noreply.github.com>
2022-07-10 00:29:53 +0800

    :sparkles: Add Item feature

--- src/components/Pages/UserDashboard.jsx
+++ src/components/Pages/UserDashboard.jsx
@@ -1,18 +1,28 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, Text, useDisclosure} from "@chakra-ui/react";
 import React from "react";
 import { PageWrapper } from "../common/PageWrapper";
 import '../../styles/pages.css'
 import { ItemList } from "../Inventory/ItemList";
+import { PrimaryButton } from "../common/Button";
+import { AddItemModal } from "../Inventory/AddItemModal";
 
 export const UserDashboard = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
   return (
-    <PageWrapper>
-      <Box className="header">
-        My Dashboard
-      </Box>
-      <hr className="header-line"/>
-      <ItemList />
-    </PageWrapper>
+    <>
+      <AddItemModal isOpen={isOpen} onClose={onClose} />
+
+      <PageWrapper>
+        <Box className="header">
+          <Flex direction="row" justifyContent={'space-between'} alignContent={'center'}>
+            <Text>My Dashboard</Text>
+            <PrimaryButton text={'Add'} fn={onOpen} />
+          </Flex>
+        </Box>
+        <hr className="header-line"/>
+        <ItemList />
+      </PageWrapper>
+    </>
   )
 }
