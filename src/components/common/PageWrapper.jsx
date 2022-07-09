9a7ef1e60122838cca5737eb3d04e31fbc69f371
Marcus Ong <58963565+marcusbory@users.noreply.github.com>
2022-07-09 22:44:51 +0800

    :lipstick: Update PageWrapper css

--- src/components/common/PageWrapper.jsx
+++ src/components/common/PageWrapper.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export const PageWrapper = ({ children }) => {
   return (
     <Center mt="80px">
-      <Box minW="350px" maxW="600px">
+      <Box minW="350px" maxW="600px" w="100%">
         { children }
       </Box>
     </Center>
