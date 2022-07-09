296a481ce6fcb861bcb859a4c084970a4964a726
Mayank <mayank.panjiyara2002@gmail.com>
2022-07-10 03:01:27 +0800

    Add delete button

--- src/components/Inventory/ItemCard.jsx
+++ src/components/Inventory/ItemCard.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
-import { Box, Flex, useDisclosure  } from '@chakra-ui/react'
-import {EditIcon} from '@chakra-ui/icons'
+import { Box, Flex, HStack, useDisclosure  } from '@chakra-ui/react'
+import {EditIcon, DeleteIcon} from '@chakra-ui/icons'
 import '../../styles/component.css'
 import { UpdateItemModal } from './UpdateItemModal'
+import { db } from "../../firebase";
+import {doc, updateDoc, deleteDoc} from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext";
 
 export const ItemCard = ({ item, key }) => {
+  let navigate = useNavigate()
   const { isOpen, onOpen, onClose } = useDisclosure()
+  const { currentUser } = useAuth()
   const expiry = new Date(item.expiry.toDate()).toLocaleDateString('en-SG')
+
+  const deleteItem = async(e) => {
+    e.preventDefault()
+    const ref = doc(db.collection('users').doc(currentUser.email), "items", item.id)
+    await deleteDoc(ref)
+    navigate('/')
+  }
+
   return (
     <>
     <UpdateItemModal isOpen={isOpen} onClose={onClose} item={item}/>
@@ -16,7 +30,10 @@ export const ItemCard = ({ item, key }) => {
       <Box>
         {item.name}
       </Box>
+      <HStack>
       <EditIcon onClick={onOpen} />
+      <DeleteIcon onClick={deleteItem}/>
+      </HStack>
       </Flex>
       <hr />
       <Flex dir="row" justifyContent={'space-between'}>
