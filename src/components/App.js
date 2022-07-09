0d5a6f6d9a08f0d07a36a1aaf7e8a3bf15e64951
Marcus Ong <58963565+marcusbory@users.noreply.github.com>
2022-07-09 22:44:08 +0800

    :zap: Update routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter as Router, Routes as Switch, Route } from 'react-router-dom'
-import { PageWrapper } from './common/PageWrapper';
 import { AuthProvider } from '../contexts/AuthContext';
-import { PrivateRoute } from './Auth/PrivateRoute';
 import { SignUp } from './Auth/Signup';
 import { HomePage } from './Pages/HomePage';
 import { Login } from './Auth/Login';
+import '../styles/index.css'
 
 function App() {
   return (
@@ -15,9 +14,9 @@ function App() {
         <AuthProvider>
           <Switch>
             <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
             {/* <Route path="/home" element={<PrivateRoute component={<HomePage />} />} /> */}
-            <Route path="/home" element={<HomePage />} />
-            <Route exact path="/" element={<Login />} />
+            <Route exact path="/" element={<HomePage />} />
           </Switch>
         </AuthProvider>
       </Router>
